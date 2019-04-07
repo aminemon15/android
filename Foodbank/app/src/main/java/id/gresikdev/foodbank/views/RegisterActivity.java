e8fb3e7666b3b37a0e59aeea7f99e360a243d94f
aminemon15 <aminsyifak15@gmail.com>
2019-04-07 13:09:14 +0700

    Pengiriman pertama

---
+++ Foodbank/app/src/main/java/id/gresikdev/foodbank/views/RegisterActivity.java
@@ -0,0 +1,15 @@
+package id.gresikdev.foodbank.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.gresikdev.foodbank.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
